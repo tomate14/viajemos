eb74b6dd53ebb99175899eda3bc959c1cf56a7ca
Maxi Roselli <maxiroselli@gmail.com>
2019-10-28 14:41:38 -0300

    Estructura de proyectos de viajemos

---
+++ backend/viajemos/src/main/java/com/example/viajemos/ViajemosApplication.java
@@ -0,0 +1,13 @@
+package com.example.viajemos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViajemosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViajemosApplication.class, args);
+	}
+
+}
---
+++ backend/viajemos/src/test/java/com/example/viajemos/ViajemosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.viajemos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViajemosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
